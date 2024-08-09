4ca4ee8e1cf6b9c8d1471897e921fad78be45f93
Theus <matheusgoias@hotmail.com>
2024-08-09 17:30:13 +0100

    Added Movies lib

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/* Node.js server setup using json-server for creating a mock REST API from a JSON file.
+This server will serve the data in db.json as if it were a real API, making it useful for prototyping or testing */
+
 require("dotenv").config();
 const jsonServer = require("json-server");
 const morgan = require("morgan");
